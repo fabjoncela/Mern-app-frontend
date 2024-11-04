335becd203abc34624aaf414f409c647aaeb1156
fabjoncela <fabjoncela@gmail.com>
2024-11-04 12:39:44 +0100

    useState for input, left at 14,50

--- src/App.js
+++ src/App.js
@@ -1,5 +1,66 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 function App() {
-  return <div className="App">hi</div>;
+  //State
+  const [notes, setNotes] = useState(null);
+  const [createForm, setCreateForm] = useState({
+    title: "",
+    body: "",
+  });
+
+  //Use effect
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  //Functions
+  const fetchNotes = async () => {
+    //fetch the notes
+    const res = await axios.get("http://localhost:3000/notes");
+    //set to state
+    setNotes(res.data.notes);
+  };
+
+  const updateCreateFormField = (e) => {
+    const { name, value } = e.target;
+    setCreateForm({...createForm,  [name]: value}); 
+
+  };
+
+  return (
+    <div className="App">
+      <div className="get-notes">
+        <h2>Notes:</h2>
+        {notes &&
+          notes.map((note) => {
+            return (
+              <div key={note._id}>
+                <h3>{note.title}</h3>
+              </div>
+            );
+          })}
+      </div>
+      <div>
+        <h2>Create Note</h2>
+        <form>
+          <input
+            onChange={updateCreateFormField}
+            value={createForm.title}
+            type="text"
+            name="title"
+            placeholder="Title"
+          />
+          <textarea
+            onChange={updateCreateFormField}
+            value={createForm.body}
+            name="body"
+          />
+          <button type="submit">Create note</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default App;
